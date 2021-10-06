9fba78775fcd8b16653cb4c1f1a8529e57c53589
kmh0818 <michaelkim0818@gmail.com>
2021-10-06 16:19:07 +0900

    ok

---
+++ server/migrations/20211006064903-create-not-to-do-list.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('NotToDoLists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      list: {
+        type: Sequelize.STRING
+      },
+      planned_time: {
+        type: Sequelize.INTEGER
+      },
+      time: {
+        type: Sequelize.INTEGER
+      },
+      theme: {
+        type: Sequelize.STRING
+      },
+      checkBox: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('NotToDoLists');
+  }
+};
